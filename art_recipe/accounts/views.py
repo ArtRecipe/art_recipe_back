fd718c5f552ebe8f488d70d01eb1b6d29729fc7d
seol <suljiwon@naver.com>
2021-08-21 23:44:12 +0900

    Feat: profile models, serializers, views, urls

--- art_recipe/accounts/views.py
+++ art_recipe/accounts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.views import APIView
+from rest_framework import mixins
+from .models import User, Profile
+from .serializers import (
+    UserSerializer,
+    ProfileSerializer,
+)
 
-# Create your views here.
+class ProfileViewSet(GenericViewSet, mixins.UpdateModelMixin, mixins.CreateModelMixin):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+class UserView(APIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = self.serializer_class(request.user)
+        return Response(serializer.data)
