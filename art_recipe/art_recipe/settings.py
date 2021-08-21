d2c8281fb1ea6fdcbc67f9b753558268e55991c9
seol <suljiwon@naver.com>
2021-08-22 07:00:00 +0900

    Feat: Add ALLOWED_HOSTS for deployment

--- art_recipe/art_recipe/settings.py
+++ art_recipe/art_recipe/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '0.0.0.0', '115.85.183.95']
 
 
 # Application definition
