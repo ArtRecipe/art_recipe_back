6ee3d788ad10508405b2cc20b7892423a40b5f27
seol <suljiwon@naver.com>
2021-08-21 23:44:23 +0900

    Feat: root urls

--- art_recipe/art_recipe/urls.py
+++ art_recipe/art_recipe/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/oauth/', include('allauth.urls')),
+    path("api/accounts/", include("accounts.urls")),
+    path("api/post/", include("post.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
