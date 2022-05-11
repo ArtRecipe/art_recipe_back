e3561756fc8958d6d8b0c7072cefb2cf4abb8c9e
seol <suljiwon@naver.com>
2022-05-12 02:00:16 +0900

    Feat: add post image to admin

--- art_recipe/post/admin.py
+++ art_recipe/post/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post, Material
+from .models import Post, Material, PostImage
 
 admin.site.register(Post)
 admin.site.register(Material)
+admin.site.register(PostImage)
