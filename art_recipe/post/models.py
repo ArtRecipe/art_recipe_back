12341fa1582e7395104e313e6d8e3ecff90ab798
seol <suljiwon@naver.com>
2022-05-12 01:58:39 +0900

    Feat: improve post model, serializer one to many relation

--- art_recipe/post/models.py
+++ art_recipe/post/models.py
@@ -2,21 +2,33 @@ from django.db import models
 from accounts.models import User
 
 
-class Material(models.Model):
-    name=models.CharField(max_length=64)
-    url=models.URLField(blank=True)
 
 class Post(models.Model):
     writer=models.ForeignKey(User, related_name="post", on_delete=models.CASCADE)
     title=models.CharField(max_length=128)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    bookmarks=models.ManyToManyField(User, related_name="bookmarked_post", blank=True)
-    thumbnail = models.FileField(upload_to="files/")
-    material=models.ManyToManyField(Material, related_name="post", blank=True)
+    bookmarkers=models.ManyToManyField(User, related_name="bookmarked_post", blank=True)
     color=models.CharField(max_length=256, blank=True)
     desc = models.TextField(blank=True)
+    url = models.URLField(blank=True)
+
+    @property
+    def bookmarks(self):
+        return self.bookmarkers.count()
+
+    def bookmark(self, user):
+        if user in self.bookmarkers.all():
+            self.bookmarkers.remove(user)
+        else:
+            self.bookmarkers.add(user)
+        self.save()
 
 class PostImage(models.Model):
-    post=models.ForeignKey(Post, on_delete=models.CASCADE)
+    post=models.ForeignKey(Post, related_name="images", on_delete=models.CASCADE)
     image=models.FileField(upload_to="files/")
+
+class Material(models.Model):
+    post=models.ForeignKey(Post, related_name="materials", on_delete=models.CASCADE)
+    name=models.CharField(max_length=64)
+    url=models.URLField(blank=True)
--- art_recipe/post/serializers.py
+++ art_recipe/post/serializers.py
@@ -6,27 +6,42 @@ from accounts.serializers import UserSerializer
 class MaterialSerializer(serializers.ModelSerializer):
     class Meta:
         model = Material
-        fields="__all__"
-
-class PostSerializer(serializers.ModelSerializer):
-    writer=UserSerializer()
-    material=MaterialSerializer()
-
-    class Meta:
-        model = Post
-        fields=["id","writer", "title", "created_at", "updated_at", "thumbnail", "material", "color", "desc"]
-        read_only_fields=["id", "created_at", "updated_at"]
-
-class PostCreateSerializer(serializers.ModelSerializer):
-    writer = serializers.HiddenField(default=serializers.CurrentUserDefault())
-
-    class Meta:
-        model = Post
-        fields = ["id","writer", "title", "created_at", "updated_at", "thumbnail", "material", "color", "desc"]
-        read_only_fields = ["id", "created_at", "updated_at"]
-
+        fields = ["name", "url"]
 
 class PostImageSerializer(serializers.ModelSerializer):
     class Meta:
-        model = PostImage
-        fields = "__all__"
+        model = PostImage   
+        fields = ["image"]
+
+class PostSerializer(serializers.ModelSerializer):
+    writer=UserSerializer()
+    images=PostImageSerializer(many = True, read_only = True)
+    materials=MaterialSerializer(many = True, read_only = True)
+
+    class Meta:
+        model = Post
+        fields=["id", "writer", "title", "created_at", "updated_at", "color", "desc", "url", "bookmarks", "images", "materials"]
+        read_only_fields=["id", "writer", "created_at", "updated_at"]
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    writer = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    materials = MaterialSerializer(many = True)
+
+    def create(self, validated_data):
+        materials_data = validated_data.pop('materials')
+        images_data = self.context["request"].FILES
+
+        post = Post.objects.create(**validated_data)
+
+        for image_data in images_data.getlist('image'):
+            PostImage.objects.create(post = post, **image_data)
+        
+        for material_data in materials_data:
+            Material.objects.create(post = post, **material_data)
+
+        return post
+
+    class Meta:
+        model = Post
+        fields =["id", "writer", "title", "created_at", "updated_at", "color", "desc", "url", "materials"]
+        read_only_fields = ["id", "writer", "created_at", "updated_at"]
