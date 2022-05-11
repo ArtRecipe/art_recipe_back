ff406c6f4e9b04831e647e32a197b897791aef75
seol <suljiwon@naver.com>
2022-05-12 01:59:43 +0900

    Feat: add post filter, search filter

--- art_recipe/post/views.py
+++ art_recipe/post/views.py
@@ -1,22 +1,15 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import CreateAPIView
+from rest_framework import decorators
 from .models import Post, Material, PostImage
 from .serializers import (
-    MaterialSerializer,
     PostSerializer,
-    PostImageSerializer,
     PostCreateSerializer
 )
 from .permissions import (
     IsPostWriter,
 )
-
-
-class MaterialViewSet(ModelViewSet):
-    queryset = Material.objects.all()
-    serializer_class = MaterialSerializer
-
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
@@ -31,10 +24,9 @@ class PostViewSet(ModelViewSet):
         "update": [IsPostWriter],
         "destroy": [IsPostWriter],
     }
-    template_names = {
-        "list": ['post_list.html'],
-        "retrieve": ['post_detail.html'],
-    }
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['writer', 'bookmarkers']
+    search_fields = ['title', 'writer__username', 'desc', 'materials__name']
 
     def get_permissions(self):
         return [permission() for permission in self.permission_classes.get(self.action, [])]
@@ -42,8 +34,10 @@ class PostViewSet(ModelViewSet):
     def get_serializer_class(self):
         return self.serializer_classes.get(self.action, self.serializer_class)
 
+    @decorators.action(detail=True, methods=["POST"])
+    def bookmark(self, request, *args, **kwargs):
+        post = self.get_object()
+        post.bookmark(request.user)
+        return self.retrieve(request, *args, **kwargs)
 
-class PostImageView(CreateAPIView):
-    queryset = PostImage.objects.all()
-    serializer_class = PostImageSerializer
-    lookup_field = "id"
+    
